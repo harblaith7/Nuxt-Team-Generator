1dbb1f3097a3754271b87bd9bc2be82f06382686
Laith Harb <harblaith77@gmail.com>
2020-06-19 16:15:51 -0400

    Added functional navbar that renders multiple pages

---
+++ components/shared/AppNav.vue
@@ -0,0 +1,27 @@
+<template>
+    <nav class="box-border bg-indigo h-20">
+        <ul class="h-full flex items-center justify-between px-10">
+            <li class="cursor-pointer">
+                <nuxt-link to="/">Todoey</nuxt-link>
+            </li>
+            <div class="flex">
+                <li class="mr-8 cursor-pointer">
+                    <nuxt-link to="/about">About</nuxt-link>
+                </li>
+                <li class="mr-8 cursor-pointer">
+                    <nuxt-link to="/todo">Todo</nuxt-link>
+                </li>
+            </div>
+        </ul>
+    </nav>
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/*
+** TailwindCSS Configuration File
+**
+** Docs: https://tailwindcss.com/docs/configuration
+** Default: https://github.com/tailwindcss/tailwindcss/blob/master/stubs/defaultConfig.stub.js
+*/
+const colors = {
+  indigo: '#5c6ac4',
+  blue: '#007ace',
+  red: '#de3618',
+}
+
+module.exports = {
+  theme: {
+    colors
+  },
+  variants: {},
+  plugins: []
+}
